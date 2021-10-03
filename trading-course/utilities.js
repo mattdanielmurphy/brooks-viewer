ec68805cd581520bc091d64966ebd512bb16b08d
Matthew Murphy <matt@mattmurphy.ca>
2021-10-03 00:23:02 -0600

    add wrapper for html files with basic formatting

--- trading-course/utilities.js
+++ trading-course/utilities.js
@@ -14,4 +14,27 @@ async function writeLocalFile(pathName, content) {
 	fs.writeFile(path.join(__dirname, saveDir, pathName), content)
 }
 
-module.exports = { fetchHtml, writeLocalFile }
+function removeInlineColorInitial(htmlContent) {
+	return htmlContent.replace('color: initial;', '')
+}
+
+function wrapHtml(htmlContent, title = 'Document') {
+	htmlContent = removeInlineColorInitial(htmlContent)
+	return `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>${title}</title>
+		<style rel="stylesheet" type="text/css">
+			body {
+				font-family: 'Calibri', sans-serif;
+				max-width: 50rem;
+				margin: 5rem auto;
+			}
+			@media (prefers-color-scheme: dark) { 
+				body { background: #222; color: #eee; }
+			}
+		</style></head>
+		<body>${htmlContent}</body></html>`
+}
+
+module.exports = { fetchHtml, writeLocalFile, wrapHtml }
