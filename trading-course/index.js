8beaf09567084db6298074920462be1809345e61
Matthew Murphy <matt@mattmurphy.ca>
2021-10-05 19:51:16 -0600

    add more progress comments

--- trading-course/index.js
+++ trading-course/index.js
@@ -29,9 +29,13 @@ async function getBlogPostUrlsForPage(url) {
  */
 async function getBlogPostUrlsForNumberOfPages(url, pages = 1) {
 	const urls = { pages: [] }
+	console.log(`1/2 | Getting blog post URLs for page 1/${pages}...`)
 	urls.pages.push(await getBlogPostUrlsForPage(url))
 	if (pages > 1) {
 		for (let pageNumber = 2; pageNumber <= pages; pageNumber++) {
+			console.log(
+				`1/2 | Getting blog post URLs for page ${pageNumber} of ${pages}...`,
+			)
 			urls.pages.push(await getBlogPostUrlsForPage(`${url}/page/${pageNumber}`))
 		}
 	}
@@ -88,13 +92,15 @@ async function downloadImages(html, folderPath, i) {
 					folderPath,
 					`${i}-${fileName}`,
 				)
-				console.log(filePath)
 				await downloadImage({
 					url: imageUrl,
 					dest: filePath,
 				})
 
 				html = html.replace(re, `<img src="${filePath}"/>`)
+				// ! NEED TO FIX: (not removing image tag)
+				// ! NEED TO FIX: (not removing image tag)
+				// ! NEED TO FIX: (not removing image tag)
 				// ! NEED TO FIX: (not removing image tag)
 				text = htmlToText(html.replace(re, ''))
 			}
@@ -127,11 +133,11 @@ async function getContentFromPage(url) {
 			return sectionText
 		}),
 	)
-	// console.log(text)
 }
 
 async function getContentFromPages({ pages }) {
-	pages.forEach(async (page) => {
+	pages.forEach(async (page, i) => {
+		console.log(`Getting content for page ${i} of ${pages.length}...`)
 		return await page.forEach(async (url) => await getContentFromPage(url))
 	})
 }
@@ -142,15 +148,6 @@ async function getContentFromBlog() {
 		'https://www.brookstradingcourse.com/blog/market-update',
 		3,
 	)
-	// const urls = {
-	// 	pages: [
-	// 		[
-	// 			'https://www.brookstradingcourse.com/market-update/emini-september-rally/',
-	// 		],
-	// 	],
-	// }
-	// const content = await getContentFromPage(urls.pages[0][0])
-	// const testHtml = `<img loading="lazy" width="680" height="383" src="https://www.brookstradingcourse.com/wp-content/uploads/2021/09/Emini-bull-trend-from-the-open-and-then-parabolic-wedge-midday-bear-trend-reversal-680x383.png" alt="" class="wp-image-122221" srcset="https://www.brookstradingcourse.com/wp-content/uploads/2021/09/Emini-bull-trend-from-the-open-and-then-parabolic-wedge-midday-bear-trend-reversal-680x383.png 680w, https://www.brookstradingcourse.com/wp-content/uploads/2021/09/Emini-bull-trend-from-the-open-and-then-parabolic-wedge-midday-bear-trend-reversal-300x169.png 300w, https://www.brookstradingcourse.com/wp-content/uploads/2021/09/Emini-bull-trend-from-the-open-and-then-parabolic-wedge-midday-bear-trend-reversal-768x432.png 768w, https://www.brookstradingcourse.com/wp-content/uploads/2021/09/Emini-bull-trend-from-the-open-and-then-parabolic-wedge-midday-bear-trend-reversal-1536x864.png 1536w, https://www.brookstradingcourse.com/wp-content/uploads/2021/09/Emini-bull-trend-from-the-open-and-then-parabolic-wedge-midday-bear-trend-reversal.png 1920w" sizes="(max-width: 680px) 100vw, 680px"></a></figure><h3>End of day summary</h3><ul><li>Bull Trend From The Open to far above 50-day MA. </li><li>After breakout phase, there was double bottom bull flag and a triangle. This led to a bull channel to just below the measured move target based on the height of the Triangle.</li><li>The Emini had a midday reversal down from a parabolic wedge buy climax.</li><li>99.95% of days have at least 3 bars with lows below the EMA, which happened today.</li><li>75% of the time, there is then a test of the day’s high after the 20-Gap Bar buy setup. That did not happen today. </li><li>Also, 60% chance Moving Average Gap Bars in a bull trend will lead to a test of the high. That also failed.</li><li>Instead, the selloff to the EMA was also a test of the apex of the earlier triangle. The reversal down was a Midday Bear Trend Reversal, and not just a minor reversal down to the EMA.</li><li>On the daily chart, 4-day rally, but lacked consecutive big bull bars closing on their highs. It therefore looks more like a leg in a trading range than a resumption of the bull trend.</li><li>If there are several bear bars over the next several days, the odds that the 15% correction has begun will remain at 50%.</li><li>If the bulls begin to get bull bars closing near their highs, the 18-month bull trend would then likely be resuming. The odds of a new high before a 15% correction would go up to 60%.</li></ul><p><em>See the <a rel="noreferrer noopener" href="https://www.brookstradingcourse.com/blog/analysis/" target="_blank">weekly update</a> for a discussion of the price action on the weekly chart and for what to expect going into next week.</em></p>`
 	await getContentFromPages(urls)
 	console.log('Done!')
 }
